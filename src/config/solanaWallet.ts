74fdb71338217c91ef18e2b96e441ea67574e9a1
jarrydallison <jarryd.allison@gmail.com>
2025-07-28 14:15:26 -0600

    feat: fix typescript errors

--- src/config/solanaWallet.ts
+++ src/config/solanaWallet.ts
@@ -1,36 +1,41 @@
 import {
-    PhantomWalletAdapter,
-    SolflareWalletAdapter, WalletConnectWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import {clusterApiUrl} from '@solana/web3.js';
-import {useMemo} from 'react';
-import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  WalletConnectWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { Cluster, clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 export const useSolanaWalletConfig = () => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = import.meta.env.VITE_SOLANA_CLUSTER_NAME;
+  const network = import.meta.env.VITE_SOLANA_CLUSTER_NAME as Cluster;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const WC_NETWORK = network === 'mainnet-beta' ? WalletAdapterNetwork.Mainnet : WalletAdapterNetwork.Devnet;
+  const WC_NETWORK =
+    network === "mainnet-beta"
+      ? WalletAdapterNetwork.Mainnet
+      : WalletAdapterNetwork.Devnet;
 
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network: network }),
-        new WalletConnectWalletAdapter({
-            network: WC_NETWORK,
-            options: {
-                projectId: import.meta.env.VITE_WALLET_CONNECT_APP_ID,
-                metadata: {
-                    name: 'Hastra',
-                    description: 'Hastra Protocol Wallet',
-                    url: "https://hastra.io",
-                    icons: [`${import.meta.env.VITE_CDN_URL}/images/hastra-mint-logo.png`]
-                },
-            }
-        })
-
+      new SolflareWalletAdapter({ network: network as WalletAdapterNetwork }),
+      new WalletConnectWalletAdapter({
+        network: WC_NETWORK,
+        options: {
+          projectId: import.meta.env.VITE_WALLET_CONNECT_APP_ID,
+          metadata: {
+            name: "Hastra",
+            description: "Hastra Protocol Wallet",
+            url: "https://hastra.io",
+            icons: [
+              `${import.meta.env.VITE_CDN_URL}/images/hastra-mint-logo.png`,
+            ],
+          },
+        },
+      }),
     ],
     [WC_NETWORK, network]
   );
