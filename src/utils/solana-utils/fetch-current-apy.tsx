56134d43f3e4b4a1e388ba511e4d3f2ba06aba98
jarrydallison <jarryd.allison@gmail.com>
2025-07-31 11:36:02 -0600

    feat: add back FM APY for now

--- src/utils/solana-utils/fetch-current-apy.tsx
+++ src/utils/solana-utils/fetch-current-apy.tsx
@@ -1,11 +1,16 @@
+interface FigureYieldResponse {
+  rate: number;
+}
+
 export const fetchCurrentAPY = async (): Promise<number> => {
-  // TODO: Update with service backend for YLDS
-  // const response = await fetch(
-  //   "https://api.codetabs.com/v1/proxy?quest=https://www.figuremarkets.com/service-funds/public/api/v1/funds/17d885eb-13e9-47a4-ad2f-228c0aa89a91/yield"
-  // );
-  // if (!response.ok) {
-  //   throw new Error("Failed to fetch APY data");
-  // }
-  // const data: FigureYieldResponse = await response.json();
-  return 0;
+  // TODO: Replace with Solana YIELDS service when able
+  const response = await fetch(
+    "https://api.codetabs.com/v1/proxy?quest=https://www.figuremarkets.com/service-funds/public/api/v1/funds/17d885eb-13e9-47a4-ad2f-228c0aa89a91/yield"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch APY data");
+  }
+  const data: FigureYieldResponse = await response.json();
+  // Subtract 0.25% take rate
+  return data && data.rate && data.rate > 0.25 ? data.rate - 0.25 : 0;
 };
